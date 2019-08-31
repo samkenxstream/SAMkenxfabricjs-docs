efd05c3bad2723ffcaa5f59a0e35f49d85d4265c
Andrea Bogazzi <abogazzi@shutterstock.com>
2019-08-31 08:15:11 +0200

    changes in script

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'Fabric.js', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://fabricjs-docs.github.io', // Your website URL
+  url: 'https://fabricjs.github.io', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
